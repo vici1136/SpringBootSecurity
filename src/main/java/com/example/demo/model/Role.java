acd3b915b35b77f67c6074cafd65de3ae341850f
Dragoman Vicențiu <vdragoman11@gmail.com>
2026-01-08 20:36:49 +0200

    Added Spring Boot Security project

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "ROLES")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    @Column(unique = true)
+    private String role;
+
+
+}
